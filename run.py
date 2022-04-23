1faa92e98cdb45e99883a4709e0b733ffbf1b178
gladyswambura <gladyswahito7@gmail.com>
2022-04-23 13:53:01 +0300

    imports user file and creates a method that prints instructons for the user

---
+++ run.py
@@ -0,0 +1,8 @@
+from user import User
+
+def main():
+    print("Hello, welcome to password locker!")
+    print('\n')
+    print("To create a new account, use shortcode 'c' to login to your account, use shortcode 'l' to to exit the program use short code 'e'")
+    print('\n')
+    
