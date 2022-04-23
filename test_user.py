34d3d5f8193a85729dd8ab3ec0ecde32a1e73a4e
gladyswambura <gladyswahito7@gmail.com>
2022-04-23 15:11:54 +0300

    creates a method (setUp(self)) that is run before each test case/called in every function

--- test_user.py
+++ test_user.py
@@ -8,5 +8,10 @@ class Testuser(unittest.TestUser):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Gladys","1234") # create user object with username and password as the arguments
     
 
