3d0dbcb4cbb99807024c4bd2ea1c73e088c9867d
gladyswambura <gladyswahito7@gmail.com>
2022-04-23 15:16:46 +0300

    Creates our first test case  which check the initialization/creation of user instances is properly done

--- test_user.py
+++ test_user.py
@@ -13,5 +13,13 @@ class Testuser(unittest.TestUser):
         Set up method to run before each test cases.
         '''
         self.new_user = User("Gladys","1234") # create user object with username and password as the arguments
+
+# First test case
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.user_name,"Gladys")
+        self.assertEqual(self.new_user.password,"1234")
     
 
