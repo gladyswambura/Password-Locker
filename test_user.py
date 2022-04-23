c02d396865910f4cdd1f7dcbf2033cebd58f5c62
gladyswambura <gladyswahito7@gmail.com>
2022-04-23 15:04:04 +0300

    creates a test class (Testuser) that defines test cases for the class behaviours

--- test_user.py
+++ test_user.py
@@ -2,5 +2,11 @@
 import unittest         # import unittest module
 from user import User   # import User class from user.py
 
-
+class Testuser(unittest.TestUser):
+    '''
+    Test class that defines test cases for the user class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    
 
