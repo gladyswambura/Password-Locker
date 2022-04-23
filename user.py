7a9004d8fb145c66da2860dbafc3f437ca8f9ed5
gladyswambura <gladyswahito7@gmail.com>
2022-04-23 13:43:05 +0300

    generates a new instance of user class

---
+++ user.py
@@ -0,0 +1,15 @@
+class User:
+    '''
+    This class generates a new instance of user class
+    '''
+
+# user_list=[]
+# def __init__(self,first_name,last_name,password):
+#     self.first_name=first_name
+#     self.last_name=last_name
+#     self.password=password
+# def save_user(self):
+#     '''
+#     save_user method saves user objects into user_list
+#     '''
+#     User.user_list.append(self)
