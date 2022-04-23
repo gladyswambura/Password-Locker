5caacc397ed90fcfdb1e7276afd0fb093ce9f9fb
gladyswambura <gladyswahito7@gmail.com>
2022-04-23 13:57:25 +0300

    creates a method that allows us to delete a saved user in the user_list

--- user.py
+++ user.py
@@ -4,6 +4,7 @@ class User:
     '''
 
 user_list=[] # users will be appended in this list
+
 def __init__(self,first_name,last_name,password):
     '''
     Method to define the properties of each user object.
@@ -12,9 +13,15 @@ def __init__(self,first_name,last_name,password):
     self.last_name=last_name
     self.password=password
 
-
 def save_user(self):
     '''
     save_user method saves user objects into user_list
     '''
     User.user_list.append(self)
+
+def delete_user(self):
+    '''
+    delete_user method deletes a saved user from the user_list
+    '''
+    User.user_list.remove(self)
+
