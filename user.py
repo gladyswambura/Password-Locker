a187e27eb1b0ab51496aa92c06aaa90915fbdf5a
gladyswambura <gladyswahito7@gmail.com>
2022-04-23 13:50:06 +0300

    creates a method that saves user object into the user_list

--- user.py
+++ user.py
@@ -6,15 +6,15 @@ class User:
 user_list=[] # users will be appended in this list
 def __init__(self,first_name,last_name,password):
     '''
-    Method to define the properties for each user object will hold.
+    Method to define the properties of each user object.
     '''
     self.first_name=first_name
     self.last_name=last_name
     self.password=password
 
 
-# def save_user(self):
-#     '''
-#     save_user method saves user objects into user_list
-#     '''
-#     User.user_list.append(self)
+def save_user(self):
+    '''
+    save_user method saves user objects into user_list
+    '''
+    User.user_list.append(self)
